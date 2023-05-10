9c452f520974438552813475e543de1b307231ef
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-05-10 11:46:04 +0100

    Shutterstock API search works The basic authentication works, and a picture can be displayed.

--- assets/js/backendscript.js
+++ assets/js/backendscript.js
@@ -1,1 +1,34 @@
 console.log("Hello :)");
+
+var username = "QsOKrafpmPS7P17AiEnnbsVcTSZSC40Y";
+var password = "svCAQ2cLA6TauA3d";  
+
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+$.ajax
+  ({
+    type: "GET",
+    url: "https://api.shutterstock.com/v2/images/search",
+    dataType: 'json',
+    async: true,
+    data: '{}',
+    beforeSend: function (xhr){ 
+        xhr.setRequestHeader('Authorization', make_base_auth(username, password)); 
+    },
+    success: function (){
+        console.log('Thanks for your comment!'); 
+    }
+}).then(parseSearch);
+
+function parseSearch(response) {
+  console.log(response);
+  imageDiv = $("#imagediv");
+
+  img = $("<img>");
+  img.attr("src", response.data[1].assets.huge_thumb.url);
+
+  imageDiv.append(img);
+}
